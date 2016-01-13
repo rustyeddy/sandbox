1b82670d84024e0acf2f23854605b25bafc88061
Rusty Eddy <rusty@rustyeddy.com>
2016-01-13 12:02:22 -0800

    Added js101

---
+++ js101/wakeup.js
@@ -0,0 +1,9 @@
+/**
+ * Created by rusty on 12/15/15.
+ */
+setTimeout(wakeUpUser, 4000);
+function wakeUpUser() {
+    document.write("Are you going to wakeup?");
+    console.log("I just wrote a message");
+    return;
+}
