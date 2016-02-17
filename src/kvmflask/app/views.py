ac73fd8031f0ea679f0111f0f303ad9668bfd431
Rusty Eddy <rusty@rustyeddy.com>
2016-02-16 21:31:31 -0800

    Committed some libvirt changes

--- src/kvmflask/app/views.py
+++ src/kvmflask/app/views.py
@@ -8,12 +8,15 @@ import libvirt
 @app.route('/home')
 def index():
     user = {'nickname': 'Rusty'}
+
+    # Use libvirt to list the KVM domains on this host
     conn = libvirt.open("qemu:///system")
     domains = conn.listDefinedDomains()
-
     dlist = []
     for d in domains:
         dlist.append({"name": d})
+
+    # User libvert to list the LXC containers on this host
 
     return render_template('index.html',
                            user=user,
