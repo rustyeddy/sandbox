c70a23282b0818b0dddc491e2936ee8c679c8acf
Rusty Eddy <rusty@rustyeddy.com>
2016-02-16 16:21:17 -0800

    Can list kvm domains

--- src/kvmflask/app/views.py
+++ src/kvmflask/app/views.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
+
+import libvirt
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Rusty'}
+    conn = libvirt.open("qemu:///system")
+    domains = conn.listDefinedDomains()
+
+    dlist = []
+    for d in domains:
+        dlist.append({"name": d})
+
+    return render_template('index.html',
+                           user='user',
+                           title='Home',
+                           domains=dlist)
